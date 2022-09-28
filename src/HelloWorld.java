5fc74b7d4d02016183a2f7614c8d60c291340fb1
achen <annac101@nycstudents.net>
2022-09-28 08:59:55 -0400

    Tesing1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main (String[] args)
     {
-        System.out.print("HelloWorld");
+        System.out.print("HelloWorld!");
+        System.out.print(" :D");
     }
 }
